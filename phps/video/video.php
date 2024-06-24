59e4eed60ea70b0a11c75ca050352fca04dfd7b0
NanodAaa <enderddr2@outlook.com>
2024-06-24 20:43:46 +0800

    Video view page improved

--- phps/video/video.php
+++ phps/video/video.php
@@ -17,9 +17,17 @@
 <!-- Find video data -->
 <?php
 include "../nanoweb_db/conn.php";
-
-
-
+$vid = $_GET["vid"];
+$sql = "SELECT * FROM videos WHERE video_id = $vid";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $videoName = $row["video_name"];
+    $videoCollection = $row["video_collection"];
+    $videoTag = $row["video_tag"];
+    $videoFilename = $row["video_filename"];
+}
+$conn->close();
 ?>
 
 <body class="body">
@@ -28,6 +36,7 @@ include "../nanoweb_db/conn.php";
         <h1 class="mainTitle"><a href="../index.php">NanoWeb</a></h1>
     </div>
 
+    <!-- NAVIGATION -->
     <div class="navigation-container">
         <h3>NAVIGATION: <a href="../index.php?page=video">VIDEO</a> <a href="../index.php?page=manga">MANGA</a></h3>
     </div>
@@ -35,17 +44,17 @@ include "../nanoweb_db/conn.php";
     <!-- VIDEO CONTAINER -->
     <div class="video-container">
         <video id="my-video" class="video-js" controls preload="auto" width="800" height="400" data-setup="{}">
-            <source src="<?php echo ($_GET["video"]); ?>" type="video/mp4" />
+            <source src="<?php echo ($videoFilename); ?>" type="video/mp4" />
         </video>
     </div>
 
     <!-- VIDEO INFO -->
     <div class="videoinfo-container">
         <h1>
-            <?php echo ($_GET["videoname"]); ?><br>
-            Collection: <?php echo ($_GET["collection"]); ?><br>
-            ID: <?php echo ($_GET["id"]); ?><br>
-            TAG: <?php echo ($_GET["tag"]); ?><br>
+            <?php echo ($videoName); ?><br>
+            Collection: <?php echo ($videoCollection); ?><br>
+            ID: <?php echo ($vid); ?><br>
+            TAG: <?php echo ($videoTag); ?><br>
         </h1>
 
     </div>
